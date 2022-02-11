8a9505f5cb6ba26dde7646a6e86dbc32ab73a02f
pierre0210 <conandoyle.0210@gmail.com>
2022-02-11 21:57:20 +0800

    add embed message command

--- slashCommands/ping.js
+++ slashCommands/ping.js
@@ -3,8 +3,10 @@ const { MessageEmbed } = require('discord.js');
 
 async function run(client, interaction) {
     try {
+        const now = Date.now();
+        //console.log(now + " " + interaction.createdTimestamp);
         const wsEmbed = new MessageEmbed().setColor("#198964")
-            .setDescription(`**Interaction latency:** ${Date.now() - interaction.createdTimestamp} ms\n**Websocket latency:** ${client.ws.ping} ms`);
+            .setDescription(`**Interaction latency:** ${Math.abs(now - interaction.createdTimestamp)} ms\n**Websocket latency:** ${client.ws.ping} ms`);
         await interaction.reply({ embeds: [wsEmbed] });
     } catch(err) {
         console.log(err);
