53527d4bf7ad50457b0ee8146bc26a482fe68fa3
pierre0210 <conandoyle.0210@gmail.com>
2022-03-05 16:43:04 +0800

    add supported file

--- modules/redditRss/redditPost.js
+++ modules/redditRss/redditPost.js
@@ -49,7 +49,7 @@ class redditPost {
                             .addField('作者:', `[${post.author}](${this.url+post.author})`, true)
                             .addField('貼文:', `[貼文連結](${post.link})`, true)
                             .addField('發布時間:', timestamp.toString())
-                        if(imageUrl.indexOf('.jpg') != -1 || imageUrl.indexOf('.png') != -1) {
+                        if(imageUrl.indexOf('.jpg') != -1 || imageUrl.indexOf('.png') != -1 || imageUrl.indexOf('.gif') != -1 || imageUrl.indexOf('.mp4') != -1) {
                             message.setImage(imageUrl);
                         } 
                         await this.client.channels.cache.get(channel).send({ embeds: [message] });
