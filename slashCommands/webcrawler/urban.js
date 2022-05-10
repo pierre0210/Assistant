b82f5f02202a700ef760729b5ad9327120c7407e
pierre0210 <conandoyle.0210@gmail.com>
2022-05-10 14:45:28 +0800

    add urban dictionary webcrawler

---
+++ slashCommands/webcrawler/urban.js
@@ -0,0 +1,41 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const { MessageEmbed } = require('discord.js');
+//const request = require('request');
+const axios = require('axios');
+const cheerio = require('cheerio');
+
+async function run(client, interaction) {
+	const keyword = interaction.options.getString('keyword');
+	const url = `https://www.urbandictionary.com/define.php?term=${keyword}`;
+	const embed = new MessageEmbed().setColor('#03fcdb');
+
+	axios.get(url).then(async ({ data }) => {
+		const $ = cheerio.load(data);
+		const title = $(".definition.bg-white.mb-4.shadow-light > .p-5 > .mb-8.flex > h1 > a");
+		const definition = $(".definition.bg-white.mb-4.shadow-light > .p-5 > .meaning.mb-4");
+		const example = $(".definition.bg-white.mb-4.shadow-light > .p-5 > .example.italic.mb-4");
+		const author = $(".definition.bg-white.mb-4.shadow-light > .p-5 > .contributor.font-bold > a");
+
+		definition.find('br').replaceWith('\n');
+		example.find('br').replaceWith('\n');
+
+		embed.setTitle(title.first().text())
+			.setURL(url)
+			.setAuthor({ name: author.first().text(), url: `https://www.urbandictionary.com${author.first().attr("href")}` })
+			.setDescription(definition.first().text().length > 4096 ? definition.first().text().substring(0, 4093)+'...' : definition.first().text())
+			.addField('Example: ', example.first().text().length > 1024 ? example.first().text().substring(0, 1021)+'...' : example.first().text());
+		
+		await interaction.reply({ embeds: [embed] });
+	})
+	.catch(async (err) => {
+		await interaction.reply('錯誤! 請稍後再試');
+		return;
+	});
+}
+
+module.exports.data = new SlashCommandBuilder()
+	.setName('urban')
+	.setDescription('Urban dictionary')
+	.addStringOption(option => option.setName('keyword').setDescription('關鍵詞').setRequired(true));
+
+module.exports.run = run;
