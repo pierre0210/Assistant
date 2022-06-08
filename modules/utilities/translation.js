7650a6c22beedbffa1d3e32fb79f20a837506bad
pierre0210 <conandoyle.0210@gmail.com>
2022-06-08 14:38:54 +0800

    edit embed style

--- modules/utilities/translation.js
+++ modules/utilities/translation.js
@@ -8,7 +8,9 @@ async function run(client, msg, userAvatar, userNickname) {
 	const translation = msg.content;
 	const embed = new MessageEmbed().setColor('#1000ff').setTitle('直播翻譯')
 		.setAuthor({ name: userNickname, iconURL: userAvatar, url: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ' })
-		.setDescription(translation);
+		.setDescription(translation)
+		.setTimestamp();
+		
 	await client.channels.cache.get(tarChannel).send({ embeds: [embed] });
 	//await msg.reply({ content: '完成', ephemeral: true });
 }
