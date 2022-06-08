9bc760c3f42655542c3e2bb0cb9993cdb0f12b99
pierre0210 <conandoyle.0210@gmail.com>
2022-06-08 14:40:06 +0800

    edit embed style

--- modules/utilities/translation.js
+++ modules/utilities/translation.js
@@ -6,11 +6,11 @@ async function run(client, msg, userAvatar, userNickname) {
 	const configFile = require('./../../config.json');
 	const tarChannel = configFile.translationList[msg.channel.id];
 	const translation = msg.content;
-	const embed = new MessageEmbed().setColor('#1000ff').setTitle('直播翻譯')
+	const embed = new MessageEmbed().setColor('#1000ff').setTitle('即時翻譯')
 		.setAuthor({ name: userNickname, iconURL: userAvatar, url: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ' })
 		.setDescription(translation)
 		.setTimestamp();
-		
+
 	await client.channels.cache.get(tarChannel).send({ embeds: [embed] });
 	//await msg.reply({ content: '完成', ephemeral: true });
 }
